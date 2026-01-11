a4414ab0190b6392b38e50f78b515c8f1260b33d
Alexandre Buleté <bulete.alexandre@gmail.com>
2026-01-11 22:27:09 +0100

    fix: remove property declaration from trait to avoid conflict

--- src/DispatchesDomainEvents.php
+++ src/DispatchesDomainEvents.php
@@ -5,10 +5,11 @@ namespace AlexandreBulete\DddDoctrineBridge;
 use AlexandreBulete\DddFoundation\Domain\Model\RecordsEvents;
 use AlexandreBulete\DddFoundation\Application\Event\EventDispatcherInterface;
 
+/**
+ * Requires the class to have a property: EventDispatcherInterface $eventDispatcher
+ */
 trait DispatchesDomainEvents
 {
-    protected readonly EventDispatcherInterface $eventDispatcher;
-    
     /**
      * @param object&RecordsEvents $entity
      */
@@ -18,8 +19,8 @@ trait DispatchesDomainEvents
             throw new \BadMethodCallException('Entity does not implement RecordsEvents methods');
         }
 
-        if (!$this->eventDispatcher) {
-            throw new \LogicException('Event dispatcher not set');
+        if (!isset($this->eventDispatcher)) {
+            throw new \LogicException('Event dispatcher not set. Inject EventDispatcherInterface in your repository constructor.');
         }
 
         foreach ($entity->releaseEvents() as $event) {
